3d53fe73ce24db773b7237edc9a29df0e640c290
omarakamal <omaraakamal@gmail.com>
2025-03-31 12:06:58 +0300

    updated code

--- JDBC-Lesson/src/main/java/com/omar/Main.java
+++ JDBC-Lesson/src/main/java/com/omar/Main.java
@@ -3,6 +3,7 @@ package com.omar;
 import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 //    connection URL
@@ -13,47 +14,32 @@ public class Main {
     private static final String password = "1234";
     public static void main(String[] args) {
 
-        Connection connection = null;
+//        Connection connection = null;
 
+//        close: connection, statement, resultSet, preparedStatement
 
-        try {
+        try(Connection connection = DriverManager.getConnection(url,username,password);
+) {
 //            establishing the connection using our driver
-            connection = DriverManager.getConnection(url,username,password);
             System.out.println("connection successful");
 
 
 //            CRUD Operations
 //            CREATE, READ, UPDATE, DELETE
 
-            String query = "SELECT * FROM movies ORDER BY title DESC";
 
-//            creates a statement to make the query
-            Statement statement = connection.createStatement();
-//            execute the query using the statement
-            ResultSet resultSet = statement.executeQuery(query);
+//            READ: all movies
 
-            resultSet.next();
+//            List<Movie> movies = getAllMovies(connection);
 
-//    public Movie(int movieId, String title, int releaseYear, int duration, String language, double rating, int directorId, int genreId, LocalDate updatedAt) {
+//            movies.forEach(System.out::println);
 
-            ArrayList<Movie> movies = new ArrayList<>();
+//            CREATE: add 1 movie to our table
+//            insertMovie(connection,"DROP TABLE movies;",1992,9.1);
 
-            while(resultSet.next()){
-               Movie newMovie = new Movie(
-                        resultSet.getInt("movie_id"),
-                        resultSet.getString("title"),
-                        resultSet.getInt("release_year"),
-                        resultSet.getInt("duration"),
-                        resultSet.getString("language"),
-                        resultSet.getDouble("rating"),
-                        resultSet.getInt("director_id"),
-                        resultSet.getInt("genre_id"),
-                        resultSet.getDate("updated_at") ==null ? LocalDate.now() : resultSet.getDate("updated_at").toLocalDate()
-                );
+            getMovieStartingWith(connection,"T");
 
-                System.out.println(newMovie);
 
-            }
 
 
 
@@ -68,4 +54,89 @@ public class Main {
     }
 
 
+    public static List<Movie> getAllMovies(Connection connection) {
+//        Statement statement = null;
+//        ResultSet resultSet = null;
+        String query = "SELECT * FROM movies ORDER BY title DESC";
+
+        try(Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(query);) {
+
+//            creates a statement to make the query
+
+//            execute the query using the statement
+
+
+
+            ArrayList<Movie> movies = new ArrayList<>();
+
+            while (resultSet.next()) {
+                movies.add(new Movie(
+                        resultSet.getInt("movie_id"),
+                        resultSet.getString("title"),
+                        resultSet.getInt("release_year"),
+                        resultSet.getInt("duration"),
+                        resultSet.getString("language"),
+                        resultSet.getDouble("rating"),
+                        resultSet.getInt("director_id"),
+                        resultSet.getInt("genre_id"),
+                        resultSet.getDate("updated_at") == null ? LocalDate.now() : resultSet.getDate("updated_at").toLocalDate()
+                ));
+
+            }
+            return movies;
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        finally{
+//            if(resultSet != null) resultSet.close();
+//            if(statement != null) statement.close();
+        }
+
+
+    }
+
+//    pulp fiction
+//    1994
+//    DROP TABLE movies;
+
+    public static void insertMovie(Connection connection,String title, int releaseYear, double rating){
+        String query = "INSERT INTO movies(title,release_year,rating) VALUES(?, ?, ?)";
+//        String query = String.format("INSERT INTO movies(title,release_year,rating) VALUES(%s,%d,%f)",title,releaseYear,rating);
+
+//        try with resources automatically closes my prepared statement
+        try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+
+//            protects against sql injections
+            preparedStatement.setString(1,title);
+            preparedStatement.setInt(2,releaseYear);
+            preparedStatement.setDouble(3,rating);
+
+            int rowsAdded = preparedStatement.executeUpdate();
+
+            System.out.println("Rows inserted " + rowsAdded);
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+    }
+
+    public static void getMovieStartingWith(Connection connection, String startingChar){
+        String query = "SELECT * FROM movies WHERE title ILIKE ?";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            preparedStatement.setString(1, startingChar + "%");
+            try(ResultSet resultSet = preparedStatement.executeQuery()){
+                while(resultSet.next()){
+                    System.out.println(resultSet.getString("title"));
+                }
+            }
+        }
+        catch (SQLException e){
+            System.out.println(e);
+        }
+    }
+
+//    Exercise 1: Write a method that gets all the movies that have a title that starts with a letter
+//    findMovieStartingWith(Connection connection, String startingChar)
 }
